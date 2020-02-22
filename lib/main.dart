971516144abc4734324ab634ac53064b2f7086d4
Halil Yaman <40357552+halilyaman@users.noreply.github.com>
2020-02-22 14:41:01 +0300

    main menu is designed

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,112 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Container(
+        color: Colors.blueGrey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  child: Container(
+                    width: 300.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Colors.blue,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Schedules",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  child: Container(
+                    width: 300.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Colors.pink,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Course Lists",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  child: Container(
+                    width: 300.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Colors.green,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Favorites",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 100.0),
+                child: GestureDetector(
+                  child: Container(
+                    width: 300.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Colors.black,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Generate Schedule",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
